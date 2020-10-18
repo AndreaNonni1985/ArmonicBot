4ba1d63e45315a991b7f302abce2c2ce924f05b7
andrea nonni <a.nonni.1985@gmail.com>
2020-10-18 14:46:20 +0200

    test erore di sintassi indotto volontariamente

--- ArmonicBot/ArmonicBot.cs
+++ ArmonicBot/ArmonicBot.cs
@@ -183,6 +183,7 @@ namespace cAlgo.Robots
             userInterface = new GUI(this);
             userInterface.OnClickFind += OnFind;
 
+            errore
 
             //Timer.Start(TimeSpan.FromMilliseconds(50));
 
